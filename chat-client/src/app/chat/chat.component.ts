042f61378a0670165daac1483922f3119dc6e3b0
itamarattali <itamarattali@gmail.com>
2021-12-20 14:50:56 +0200

    feat: messages display locally

--- chat-client/src/app/chat/chat.component.ts
+++ chat-client/src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
@@ -6,9 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  currentMessage: string = '';
+
+  messages: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateCurrentMessage(event: any) {
+    this.currentMessage = event.target.value;
+    console.log(this.currentMessage);
+  }
+
+  handleSubmitNewMessage() {
+    this.messages.push(this.currentMessage);
   }
 }
